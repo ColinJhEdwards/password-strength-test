1d978a58f32d0d8ee71e1dae87b07515ebf176ba
Colin Edwards <ColinJhEdwards@gmail.com>
2021-09-12 20:34:28 -0700

    created a function that checks for both upper and lower case characters based off of parameters, instead of having two seperate functions for each.

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ function calculateStrength(password) {
   const weaknesses = [];
   weaknesses.push(lengthWeakness(password));
   weaknesses.push(lowerCaseWeakness(password));
+  weaknesses.push(upperCaseWeakness(password));
   return weaknesses;
 }
 
@@ -50,21 +51,34 @@ function lengthWeakness(password) {
 
 //testing lowercase weakness with regex
 function lowerCaseWeakness(password) {
+  return characterTypeWeakness(password, /[a-z]/g, "lowercase characters");
+}
+
+function upperCaseWeakness(password) {
+  return characterTypeWeakness(password, /[A-Z]/g, "uppercase characters");
+}
+
+//since checking for lower and upper case characters was similar, this function covers both basis, which is awesome
+function characterTypeWeakness(password, regex, type) {
   //check password for lowercase letters, this regex will search for all characters between a-z
   //g property makes it a global search so it will check the entire string and not just
   // the first lowercase found
-  const matches = password.match(/[a-z]/g) || [];
+  const matches = password.match(regex) || [];
   if (matches.length === 0) {
     return {
-      message: "Your password has no lowercase characters",
+      message: `Your password has no ${type}`,
       deduction: 20,
     };
   }
 
   if (matches.length <= 2) {
     return {
-      message: "Your password could have more lowercase characters",
+      message: `Your password could have more ${type}`,
       deduction: 5,
     };
   }
+}
+
+function numberWeakness(password) {
+  return characterTypeWeakness(password, /0-9/g, "numbers");
 }
