79bfa49bf6efdbf6b62543054a575d27d2c56756
Colin Edwards <ColinJhEdwards@gmail.com>
2021-09-12 20:08:31 -0700

    added comments to understand functiosn better

--- app.js
+++ app.js
@@ -2,17 +2,21 @@ const strengthMeter = document.getElementById("strength-meter");
 const passwordInput = document.getElementById("password-input");
 const reasonsContainer = document.getElementById("reasons");
 
+//when the input of the password is updated tests will be run that will determine the strength of
+// given password
 passwordInput.addEventListener("input", () => {
   const weaknesses = calculateStrength(passwordInput.value);
   console.log(weaknesses);
 });
 
+//return the result of tests such as length, etc..
 function calculateStrength(password) {
   const weaknesses = [];
-  weaknesses.push(leangthWeakness(password));
+  weaknesses.push(lengthWeakness(password));
   return weaknesses;
 }
 
+//testing password length
 function lengthWeakness(password) {
   const length = password.length;
   if (length <= 5) {
