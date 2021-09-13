155b8f8fffc7053e766904c477ef2952ddee8e12
Colin Edwards <ColinJhEdwards@gmail.com>
2021-09-12 20:17:09 -0700

    made it so messages detailing password strength will now display

--- app.js
+++ app.js
@@ -4,15 +4,30 @@ const reasonsContainer = document.getElementById("reasons");
 
 //when the input of the password is updated tests will be run that will determine the strength of
 // given password
-passwordInput.addEventListener("input", () => {
+passwordInput.addEventListener("input", updateStrengthMeter);
+
+function updateStrengthMeter() {
   const weaknesses = calculateStrength(passwordInput.value);
   console.log(weaknesses);
-});
+  let strength = 100;
+  reasonsContainer.innerHTML = "";
+  //for each weakness found subtract the deduction amount assigned to that weakness.
+  weaknesses.forEach((w) => {
+    if (w === null) return;
+    strength -= w.deduction;
+    const message = document.createElement("div");
+    message.innerText = w.message;
+    reasonsContainer.appendChild(message);
+  });
+  //style the meter accordingly to the amount of strength
+  strengthMeter.style.setProperty("--strength", strength);
+}
 
 //return the result of tests such as length, etc..
 function calculateStrength(password) {
   const weaknesses = [];
   weaknesses.push(lengthWeakness(password));
+  weaknesses.push(lowerCaseWeakness(password));
   return weaknesses;
 }
 
